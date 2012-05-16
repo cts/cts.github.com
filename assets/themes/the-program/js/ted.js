f1a161eb50ba482b013feddcf6609fabe0508a2f
Ted Benson <edward.benson@gmail.com>
2012-05-16 18:30:41 -0400

    removed fixed positioning of sidebar

--- assets/themes/the-program/js/ted.js
+++ assets/themes/the-program/js/ted.js
@@ -8,18 +8,6 @@
  * -ted
  */
 
-$(function() {
-  sidebarwidth = $(".sidebar-width").css('width');
-  bodypaddingtop = $(".navbar-fixed-top").css('height');
-  sidebarheight = $("body").css('height');
-  $('.sidebar-nav-fixed').css('width', sidebarwidth);
-  $('.sidebar-nav-fixed').css('height', sidebarheight);
-  $('body').css('paddingTop', bodypaddingtop)
-  contentmargin = parseInt(sidebarwidth)
-  $('.span-fixed-sidebar').css('marginLeft', contentmargin);
-  $('.span-fixed-sidebar').css('paddingLeft', 60);
-});
-
 $(function() {      
   window.prettyPrint && prettyPrint()
 });
